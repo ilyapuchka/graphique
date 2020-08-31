8b794d51e648e24a785bafec6559a64e66de8c5b
Ilya Puchka <ilya.puchka@transferwise.com>
2020-08-31 13:22:39 +0100

    keypath lookup cache

--- Sources/Graphique/EntityKeyPath.swift
+++ Sources/Graphique/EntityKeyPath.swift
@@ -2,9 +2,12 @@ public protocol GQLEntityKeyPath {
     init()
 }
 
+private var lookupCache = [String: Mirror.Children]()
+
 func keyPathLookup<T: GQLEntity, U>(_ keyPath: KeyPath<T, U>) -> String {
-    let mirror = Mirror(reflecting: T.GQLEntityKeyPaths())
-    let allKeyPaths = mirror.children
+    let cacheKey = String(reflecting: T.self)
+    let allKeyPaths = lookupCache[cacheKey, default: Mirror(reflecting: T.GQLEntityKeyPaths()).children]
+    lookupCache[cacheKey] = allKeyPaths
     if let label = allKeyPaths.first(where: { $0.value as? KeyPath<T, U> == keyPath })?.label {
         return label
     } else if keyPath == \T.__typename {
